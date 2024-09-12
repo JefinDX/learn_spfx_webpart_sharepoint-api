668ac5f3672b8a7961387a6f0b50874af286426a
JefinMathew <jeffdx.mathew@gmail.com>
2024-09-12 19:44:24 +0530

    Initial Commit- Code for CRUD operations using spHttpClient.

---
+++ src/webparts/spFxHttpClientDemo/components/ISpFxHttpClientDemoProps.ts
@@ -0,0 +1,18 @@
+import {
+  ButtonClickedCallback,
+  ICountryListItem
+} from '../../../models';
+
+export interface ISpFxHttpClientDemoProps {
+  spListItems: ICountryListItem[];
+  onGetListItems: ButtonClickedCallback;
+  onAddListItem: ButtonClickedCallback;
+  onUpdateListItem: ButtonClickedCallback;
+  onDeleteListItem: ButtonClickedCallback;
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
+
